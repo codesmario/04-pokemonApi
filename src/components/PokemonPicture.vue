b9fbed99d7bb0d4db3d8ea1641f19a4bd84453cb
mario.hernandezs <mario.hernandezs@salud.gob.sv>
2022-05-30 10:20:51 -0600

    cambios en imagen

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="pokemon-container">
-  <img :src="imgSrc" 
+  <img
+    v-if="!showPokemon"
+    :src="imgSrc" 
     class="hidden-pokemon"
     alt="pokemon">
-  <img :src="imgSrc" v-if="showPokemon"
+  <img 
+    v-else
+    :src="imgSrc" 
     class="fade-in"
     alt="pokemon">
 </div>
